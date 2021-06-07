1725d2977787110e44e26709a112cb36ef79cd24
Saad Moutalib <saad-moutalib@hotmail.fr>
2021-06-07 11:41:26 +0100

    fix

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ db.sequelize.sync({
   //force: true,
 });
 
-app.get("/public/user", express.static(path.join(__dirname, "/public/user")));
-app.get("/public/client", express.static(path.join(__dirname, "/public/client")));
+app.get("/public/user", express.static(path.join(__dirname, "public/user")));
+app.get("/public/client", express.static(path.join(__dirname, "public/client")));
 
 require("./routes/user.routes")(app);
 require("./routes/client.routes")(app);
